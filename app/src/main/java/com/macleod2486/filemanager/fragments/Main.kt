b2bc6b34206043392610cae2a26dc71e8d78b0c8
macleod2486 <macleod2486@gmail.com>
2019-10-17 20:25:44 -0500

    Updated copyright

--- app/src/main/java/com/macleod2486/filemanager/fragments/Main.kt
+++ app/src/main/java/com/macleod2486/filemanager/fragments/Main.kt
@@ -3,7 +3,7 @@
  *   File Manager
  *    A application to help organize your files on your device.
  *
- *    Copyright (C) 2018  Manuel Gonzales Jr.
+ *    Copyright (C) 2019  Manuel Gonzales Jr.
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
